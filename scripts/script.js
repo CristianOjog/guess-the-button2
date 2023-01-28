79d0d33e4dd2bbf64ae93e261fa87a1e0a77eb56
CristianOjog <cristianconstantin.ojog@gmail.com>
2023-01-28 14:08:21 +0200

    I simplified the logic of the 'click' event by creating a separate function for checking the pressed button and I optimized the rest of the code

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ const submitNumber = document.querySelector('.submitNumber');
 const noButtons = document.querySelector('.noButtons');
 const groupbtns = document.querySelector('.btn-group');
 const lastResult = document.querySelector('.lastResult');
+const card = document.querySelector('.card');
 
+let gameOver = false;
 let randomNumber;
 let resetButton;
 let idButton = 0;
@@ -12,7 +14,7 @@ function generateButton() {
   submitNumber.disabled = true;
   noButtons.disabled = true;
   const userInputNum = Number(noButtons.value);
-  randomNumber = randomNumber = Math.floor(Math.random() * userInputNum) + 1;
+  randomNumber = Math.floor(Math.random() * userInputNum) + 1;
   for (let i = 0; i < userInputNum; ++i) {
     let newBtn = document.createElement('button');
     newBtn.innerText="Button";
@@ -20,42 +22,47 @@ function generateButton() {
     newBtn.id = ++idButton;
     groupbtns.appendChild(newBtn);
   }
-  
   noButtons.focus();
 }
 
 submitNumber.addEventListener('click', generateButton);
 
-groupbtns.addEventListener('click', (event) => {
-  const isButton = event.target.nodeName === 'BUTTON';
-  
-  if (!isButton) {
-    return;
-  } else if(Number(event.target.id) === randomNumber) {
+groupbtns.addEventListener('click', checkClickedButton);
+
+function checkClickedButton(ev) {
+  if (!ev.target.nodeName === 'BUTTON' || gameOver) return;
+  if(Number(ev.target.id) === randomNumber) {
     lastResult.textContent = 'Congratulations! You got it right!';
     lastResult.style.backgroundColor = 'green';
-    setGameOver();
   } else {
     lastResult.textContent = '!!!GAME OVER!!!' + ` THE WINNER BUTTON WAS ${randomNumber}!`;
     lastResult.style.backgroundColor = 'red';
-    setGameOver();
   }
-});
+  setGameOver();
+}
 
 function setGameOver() {
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new game';
-  document.querySelector('.card').append(resetButton);
-  resetButton.addEventListener('click', resetGame);
+  if (!gameOver) {
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    card.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
+  }
+  gameOver = true;
 }
 
 function resetGame() {
+  resetAll();
+  gameOver = false;
+}
+
+function resetAll() {
   idButton = 0;
   lastResult.textContent = '';
-  for (let i = 0; i < Number(noButtons.value); ++i) {
-    document.querySelector('.button').remove(document.querySelector('.button'));
+  groupbtns.innerHTML = '';
+  if (resetButton) {
+    resetButton.remove();
   }
-  resetButton.parentNode.removeChild(resetButton);
   lastResult.style.backgroundColor = 'white';
   submitNumber.disabled = false;
   noButtons.disabled = false;
